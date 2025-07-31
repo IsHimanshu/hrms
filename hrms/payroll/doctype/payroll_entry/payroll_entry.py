5d0796f0daff71efdd1561f3c0e51ff9e8574d14
IsHimanshu <51610605+IsHimanshu@users.noreply.github.com>
2025-07-31 22:03:07 +0900

    set

--- hrms/payroll/doctype/payroll_entry/payroll_entry.py
+++ hrms/payroll/doctype/payroll_entry/payroll_entry.py
@@ -340,14 +340,7 @@ class PayrollEntry(Document):
 					ss.salary_structure,
 					ss.employee,
 				)
-				.where(
-					(ssd.parentfield == component_type)
-					& (ss.name.isin([d.name for d in salary_slips]))
-					& (
-						(ssd.do_not_include_in_total == 0)
-						| ((ssd.do_not_include_in_total == 1) & (ssd.do_not_include_in_accounts == 0))
-					)
-				)
+				.where((ssd.parentfield == component_type) & (ss.name.isin([d.name for d in salary_slips])))
 			).run(as_dict=True)
 
 			return salary_components
@@ -968,13 +961,6 @@ class PayrollEntry(Document):
 				& (SalarySlip.start_date >= self.start_date)
 				& (SalarySlip.end_date <= self.end_date)
 				& (SalarySlip.payroll_entry == self.name)
-				& (
-					(SalaryDetail.do_not_include_in_total == 0)
-					| (
-						(SalaryDetail.do_not_include_in_total == 1)
-						& (SalaryDetail.do_not_include_in_accounts == 0)
-					)
-				)
 			)
 		)
 
